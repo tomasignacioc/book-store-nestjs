a6f1cbd443f60956a6a429800d9397a6ff562b3e
Tomas <tomasignaciocontreras@hotmail.com>
2022-03-18 22:32:30 -0300

    user crud

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { SharedModule } from 'src/shared/shared.module';
 import { UserRepository } from './user.repository';
+import { UserService } from './user.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UserRepository])],
+    imports: [TypeOrmModule.forFeature([UserRepository]), SharedModule],
+    providers: [UserService],
 })
 export class UserModule {}
