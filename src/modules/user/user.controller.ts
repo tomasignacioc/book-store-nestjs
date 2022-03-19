003e24d4c903bbf644ed7b9c840c2b1d7c039357
Tomas <tomasignaciocontreras@hotmail.com>
2022-03-19 18:56:32 -0300

    user controller

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,41 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+
+import { UserDto } from './dto/user.dto';
+import { User } from './user.entity';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+    constructor(private readonly _userService: UserService) { }
+
+    @Get(':id')
+    async getUser(@Param() id: number): Promise<UserDto> {
+        const user = await this._userService.get(id);
+        console.log(typeof user)
+        return user;
+    }
+
+    @Get('all')
+    async getUsers(): Promise<UserDto[]> {
+        const users = await this._userService.getAll();
+        return users;
+    }
+
+    @Post()
+    async createUser(@Body() user: User): Promise<UserDto>{
+        const createdUser = await this._userService.create(user);
+        return createdUser;
+    }
+
+    @Patch(':id')
+    async updateUser(@Param() id: number, @Body() user: User): Promise<void>{
+        const updatedUser = await this._userService.update(id,user);
+        return updatedUser;
+    }
+
+    @Delete(':id')
+    async deleteUser(@Param() id: number) {
+        await this._userService.delete(id);
+        return true;
+    }
+}
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,9 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SharedModule } from 'src/shared/shared.module';
 import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
+import { UserController } from './user.controller';
 
 @Module({
     imports: [TypeOrmModule.forFeature([UserRepository]), SharedModule],
     providers: [UserService],
+    controllers: [UserController],
 })
 export class UserModule {}
